cccf9c5146025f6b4101e4dceb5233bed036d3ff
Binod Dhamala <binoddhamala969@gmail.com>
2024-01-17 10:51:48 +0545

    deployemnt

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <div>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />}></Route>
+            <Route path="/" element={<Home/>}></Route>
             <Route path="*" element={<div>404 Not Found</div>}></Route>
           </Routes>
         </div>
