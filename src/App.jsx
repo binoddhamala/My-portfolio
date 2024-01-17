e31b7093ad2ba25f14becffa1fe9954d7ba634bf
Binod Dhamala <binoddhamala969@gmail.com>
2024-01-17 11:30:08 +0545

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import Navbar from "./Pages/Home/Navbar";
 import React from "react";
-import Home from "./Pages/Home/HomeScreen";
+import Home from "./Pages/Home/HomeScreen/Index";
 
 function App() {
   
